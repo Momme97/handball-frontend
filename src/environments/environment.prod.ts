4b2cef54cb0e0c2b4f83e798c3bbc7813e92e922
Momme Ristow <momme.ristow@prodyna.com>
2022-11-27 01:08:22 +0100

    added missing token

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,8 @@
 export const environment = {
   production: true,
   strapiUrl: 'https://content.handball-region-foerde.de/',
-  mixpanelToken: '873580c4f4ae3d5c42d735e87ffab11b'
+  mixpanelToken: '873580c4f4ae3d5c42d735e87ffab11b',
+  mapbox: {
+    accessToken: 'pk.eyJ1IjoicHJlc3Nld2FydCIsImEiOiJjbGF5aXhsaXAwaDU0M3BteGJjM3QxZWZhIn0.X2MYpiScsd_b2Yt-2xjb3w'
+  }
 };
