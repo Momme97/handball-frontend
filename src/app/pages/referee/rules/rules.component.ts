58f57400557f86eafc89977abe5590a34305add6
Momme Ristow <momme.ristow@prodyna.com>
2022-12-12 19:32:23 +0100

    added qualified person list to rules page

--- src/app/pages/referee/rules/rules.component.ts
+++ src/app/pages/referee/rules/rules.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { Subscription } from 'rxjs';
 import { Apollo, gql } from 'apollo-angular';
+import { QualifiedPerson } from 'src/app/data-models/qualified-person';
 
 const GET_RULES_CMS_DATA = gql `
   query{
@@ -39,6 +40,8 @@ const GET_RULES_CMS_DATA = gql `
 })
 export class RulesComponent implements OnInit {
   private querySubscription: Subscription;
+  public AnsprechpartnerListe: QualifiedPerson[] = [];
+
   textareaData: any;
   constructor(
     private apollo: Apollo,
@@ -48,6 +51,16 @@ export class RulesComponent implements OnInit {
     this.querySubscription = this.apollo.watchQuery<any>({
       query: GET_RULES_CMS_DATA
     }).valueChanges.subscribe(({ data, loading }) => {
+      console.log(data.schiedsrichterRegelwerk.data.attributes.Ansprechpartner)
+      data.schiedsrichterRegelwerk.data.attributes.Ansprechpartner.forEach(person => {
+        this.AnsprechpartnerListe.push({
+          position: person.position,
+          name: person.Vorname,
+          surname: person.Nachname,
+          email: person.Email,
+          mobile: person.Handynummer,
+          profilImage: person.Profilbild.data?.attributes.url        })
+      })
       this.textareaData = JSON.parse(data.schiedsrichterRegelwerk.data.attributes.Textbereich);
     });
   }
