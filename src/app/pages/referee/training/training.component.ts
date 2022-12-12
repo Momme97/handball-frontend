86b15d77fcc0b351532a20d698f26100fbe61e69
Momme Ristow <momme.ristow@prodyna.com>
2022-12-12 19:49:21 +0100

    added contact person list to training page

--- src/app/pages/referee/training/training.component.ts
+++ src/app/pages/referee/training/training.component.ts
@@ -1,4 +1,31 @@
 import { Component, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { Apollo, gql } from 'apollo-angular';
+import { QualifiedPerson } from 'src/app/data-models/qualified-person';
+const GET_QUALIFIED_PERSONLIST = gql `
+query {
+  schiedsrichterFortbildung {
+    data {
+      attributes {
+        Ansprechpartner {
+          ...on ComponentPersonPerson {
+            Vorname,
+            Nachname,
+            Position,
+            Email,
+            Handynummer,
+            Profilbild {
+              data {
+                attributes{url}
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+}
+`;
 
 @Component({
   selector: 'app-training',
@@ -6,10 +33,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./training.component.scss']
 })
 export class TrainingComponent implements OnInit {
+  private querySubscription: Subscription;
+  public AnsprechpartnerListe: QualifiedPerson[] = [];
+  constructor(
+    private apollo: Apollo,
 
-  constructor() { }
+  ) { }
 
   ngOnInit(): void {
+    this.querySubscription = this.apollo.watchQuery<any>({
+      query: GET_QUALIFIED_PERSONLIST
+    }).valueChanges.subscribe(({ data, loading }) => {
+      console.log(data.schiedsrichterFortbildung.data.attributes.Ansprechpartner);
+      data.schiedsrichterFortbildung.data.attributes.Ansprechpartner.forEach(person => {
+        this.AnsprechpartnerListe.push({
+          position: person.position,
+          name: person.Vorname,
+          surname: person.Nachname,
+          email: person.Email,
+          mobile: person.Handynummer,
+          profilImage: person.Profilbild.data?.attributes.url     
+        })
+      })
+    });
   }
 
 }
